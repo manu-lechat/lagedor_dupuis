0568052c5de4ec7a2009969807a86fb9f48841fd
Dorian Simeha <doriansimeha@gmail.com>
2018-05-25 13:19:50 +0200

    Start to make youtube article section

--- site/snippets/articles_sections/builder_youtube.php
+++ site/snippets/articles_sections/builder_youtube.php
@@ -1,13 +1,8 @@
-<section class="youtube">
 <?php $youtube_url = $data->youtube() ?>
 <?php $regex_pattern = "/(youtube.com|youtu.be)\/(watch)?(\?v=)?(\S+)?/" ?>
 <?php $match ?>
 <?php if(preg_match($regex_pattern, $youtube_url, $match)): ?>
-    <?php parse_str( parse_url( $youtube_url, PHP_URL_QUERY ), $video_id ) ?>
-    <article style="background-image:url(http://img.youtube.com/vi/<?= $video_id['v'] ?>/mqdefault.jpg);"></article>
-<?php else: ?>
-    <article style="background:#b3000a;">
-        <span>Désolé, ce n'est pas une vidéo youtube</span>
-    </article>
-<?php endif ?>
+<?php parse_str( parse_url( $youtube_url, PHP_URL_QUERY ), $video_id ) ?>
+<section class="article_youtube">
+    ID DE LA VIDEO : <?php $video_id['v'] ?>
 </section>
