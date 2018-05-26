e5fcad50c58555362edb91ac363fcdb00bf7ee58
Dorian Simeha <doriansimeha@gmail.com>
2018-05-27 01:43:12 +0200

    Captions for images inside articles

--- site/snippets/articles_sections/builder_picture.php
+++ site/snippets/articles_sections/builder_picture.php
@@ -1,5 +1,8 @@
 <?php if ($data->picture()->isNotEmpty()): ?>
 <section class="article_picture">
     <img src="<?= $page->image($data->picture())->url() ?>" alt="#">
+    <?php if( $data->caption() != '' ): ?>
+    <p><?php echo $data->caption() ?></p>
+    <?php endif ?>
 </section>
 <?php endif ?>
