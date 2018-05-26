7f29c840acb8f8fc756fd277834465ce8c6edb8f
Dorian Simeha <doriansimeha@gmail.com>
2018-05-26 20:22:56 +0200

    Better paths for assets

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,19 +1,16 @@
 <?php snippet('header') ?>
 
-
-
-
 <main  class="barba-container page_container" data-namespace="homePage">
 
 	<section class="homepage layout_display_container">
 		<a class="bt_welcome" id="bt_welcome" href="#"><?php echo $site->home_welcome()->html() ?></a>
-		<img src="/assets/img/_<?php echo($site->language())?>/home_logo.svg" class="home_logo">
-		<img src="/assets/img/_<?php echo($site->language())?>/home_logo_left.svg" class="home_logo_air_libre">
-		<img src="/assets/img/_<?php echo($site->language())?>/home_logo_right.svg" class="home_logo_france_inter">
+		<img src="<?php echo kirby()->urls()->assets() ?>/img/_<?php echo($site->language())?>/home_logo.svg" class="home_logo">
+		<img src="<?php echo kirby()->urls()->assets() ?>/img/_<?php echo($site->language())?>/home_logo_left.svg" class="home_logo_air_libre">
+		<img src="<?php echo kirby()->urls()->assets() ?>/img/_<?php echo($site->language())?>/home_logo_right.svg" class="home_logo_france_inter">
 		<div class="card-wrapper">
 			<div class="card" id="card">
-				<img src="/assets/img/homepage/couv_00.jpg" class="couv_00 plan1">
-				<img src="/assets/img/homepage/couv_01.png" class="couv_01 plan2">
+				<img src="<?php echo kirby()->urls()->assets() ?>/img/homepage/couv_00.jpg" class="couv_00 plan1">
+				<img src="<?php echo kirby()->urls()->assets() ?>/img/homepage/couv_01.png" class="couv_01 plan2">
 			</div>
 		</div>
 	</section>
@@ -25,9 +22,5 @@
 		</ul>
 	</div>
 </main>
-
-
-
-
 
 <?php snippet('footer') ?>
