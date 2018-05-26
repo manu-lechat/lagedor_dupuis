e6e8881d1c6f20b0931fdf8e582bd650141739dd
Dorian Simeha <doriansimeha@gmail.com>
2018-05-26 12:10:50 +0200

    Do the youtube article section

--- site/snippets/articles_sections/builder_youtube.php
+++ site/snippets/articles_sections/builder_youtube.php
@@ -4,6 +4,8 @@
 <?php if(preg_match($regex_pattern, $youtube_url, $match)): ?>
 <?php parse_str( parse_url( $youtube_url, PHP_URL_QUERY ), $video_id ) ?>
 <section class="article_youtube">
-    ID DE LA VIDEO : <?php $video_id['v'] ?>
+    <div class="video-container">
+		<iframe src="https://www.youtube.com/embed/<?php echo $video_id['v'] ?>" width="800" height="450" frameborder="0"></iframe>
+	</div>
 </section>
 <?php endif; ?>
